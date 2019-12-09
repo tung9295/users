1b93d7f09b6558ccbb7090d91fe4afa41a86001c
Tung windowPC <tung9295@gmail.com>
2019-12-09 16:22:30 +0900

    add my-project-users

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+require('dotenv').config();
+var pug = require('pug');
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URL);
+var User = require('./models/user_models');
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+    res.render('index_users');
+});
+
+app.get('/users/list', function(req, res) {
+    res.render('list_users');
+});
+
+app.get('/users/create', function(req, res) {
+    res.render('create_user');
+});
+
+app.get('/users/delete', function(req, res) {
+    res.render('delete_user');
+});
+
+app.listen(port, function() {
+    console.log('App listening on port ' + port);
+});
